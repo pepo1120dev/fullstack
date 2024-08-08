e0ed7154bf25cdf04f4088c5eeaa47f5ab7b5941
Richard Sarmiento <85804229+pepo1120dev@users.noreply.github.com>
2024-08-08 09:59:23 -0500

    qwert
    qwertt

--- server/server/index.js
+++ server/server/index.js
@@ -8,21 +8,30 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "http://192.168.12.21:5173",
+    ],
   })
 );
 
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "http://192.168.12.21:5173",
+    ],
     methods: ["GET", "POST"],
   },
 });
 
 const REST_API_BASE_URL =
-  "http://localhost:8080/ServiciosRest/resources/sinnombre";
+  "http://192.168.12.21:8080/ServiciosRest/resources/sinnombre";
 
+// Endpoint para obtener la lista de partes
 app.get("/items", async (req, res) => {
   try {
     const response = await axios.get(`${REST_API_BASE_URL}/listarPartes`);
@@ -32,21 +41,39 @@ app.get("/items", async (req, res) => {
   }
 });
 
+// Manejo de conexiones de Socket.IO
 io.on("connection", (socket) => {
   console.log("A user connected");
 
+  // Unirse a una sala de organigrama
   socket.on("join-orgchart", (orgChartId) => {
     socket.join(orgChartId);
     console.log(`User joined org chart ${orgChartId}`);
     console.log("Current rooms for this socket:", socket.rooms);
   });
 
+  // Salir de una sala de organigrama
   socket.on("leave-orgchart", (orgChartId) => {
     socket.leave(orgChartId);
     console.log(`User left org chart ${orgChartId}`);
     console.log("Current rooms for this socket after leaving:", socket.rooms);
   });
 
+  // Unirse a una sala de SITREP
+  socket.on("join-sitrep", (sitrepId) => {
+    socket.join(sitrepId);
+    console.log(`User joined SITREP ${sitrepId}`);
+    console.log("Current rooms for this socket:", socket.rooms);
+  });
+
+  // Salir de una sala de SITREP
+  socket.on("leave-sitrep", (sitrepId) => {
+    socket.leave(sitrepId);
+    console.log(`User left SITREP ${sitrepId}`);
+    console.log("Current rooms for this socket after leaving:", socket.rooms);
+  });
+
+  // Emitir una actualización de organigrama
   socket.on("update-orgchart", (orgChartId, updatedOrgChart) => {
     try {
       console.log(`Emitting update to room ${orgChartId}`);
@@ -56,24 +83,40 @@ io.on("connection", (socket) => {
     }
   });
 
+  socket.on("update-sitrep", (sitrepId, updatedSitrep) => {
+    try {
+      console.log(`Emitting update to room ${sitrepId}`);
+      io.to(sitrepId).emit("sitrep-updated", JSON.parse(updatedSitrep));
+    } catch (error) {
+      console.error("Error emitting updated org chart", error);
+    }
+  });
+
+  // Emitir un nuevo elemento de tabla
   socket.on("new-table-item", async (newItem) => {
     try {
       console.log("Received new table item:", newItem);
-      // Aquí puedes agregar lógica para guardar el nuevo item en la base de datos si es necesario
-      // await axios.post(`${REST_API_BASE_URL}/ruta/para/guardar`, newItem);
-
-      // Emitir el nuevo item a todos los clientes conectados
       io.emit("table-item-added", newItem);
     } catch (error) {
       console.error("Error adding new table item", error);
     }
   });
+  socket.on("new-sitrep-item", async (newSitrep) => {
+    try {
+      console.log("Received new sitrep item:", newSitrep);
+      io.emit("sitrep-item-added", newSitrep);
+    } catch (error) {
+      console.error("Error adding new sitrep item", error);
+    }
+  });
 
+  // Manejo de la desconexión del usuario
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
 });
 
+// Iniciar el servidor
 server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log("Server is running on http://192.168.12.21:3000");
 });
